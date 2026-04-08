3b74cbbfcc099a2d05f0f5d6e37c412d816b12cf
2ndStar.niki <ocknsn@gmai.com>
2026-04-08 20:19:07 +0900

    add relay server

---
+++ server/relay-server.mjs
@@ -0,0 +1,39 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+import http from "http";
+
+const app = express();
+app.use(express.json());
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+let latestTelemetry = null;
+
+wss.on("connection", (ws) => {
+  console.log("client connected");
+
+  if (latestTelemetry) {
+    ws.send(JSON.stringify(latestTelemetry));
+  }
+});
+
+app.post("/telemetry", (req, res) => {
+  latestTelemetry = req.body;
+
+  const msg = JSON.stringify(latestTelemetry);
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === 1) {
+      client.send(msg);
+    }
+  });
+
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 8787;
+
+server.listen(PORT, () => {
+  console.log("relay server running on", PORT);
+});
